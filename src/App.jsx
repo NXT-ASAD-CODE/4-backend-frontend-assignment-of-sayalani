d7107e4a7d52abfd588607680dac191e58f05025
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-13 18:33:22 +0500

    Making Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import AlertBar from './components/AlertBar'
+import PrimarySearchAppBar from './components/Navbar'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <div>
       <AlertBar/>
+      <PrimarySearchAppBar/> 
+      <LandingPage/>
     </div>
   )
 }
