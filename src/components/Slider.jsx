8268605e298d7354db0a4be93b95c0c66a46eb8e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-15 18:56:15 +0500

    Rendering Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Slider() {
   return (
-    <div>Slider</div>
+    <div className='Slider'>
+        <img src="../public/Vector (2).png" alt="" />
+        <img src="../public/Vector (3).png" alt="" />
+        <img src="../public/Vector (4).png" alt="" />
+        <img src="../public/Vector (5).png" alt="" />
+        <img src="../public/Vector (6).png" alt="" />
+    </div>
   )
 }
 
