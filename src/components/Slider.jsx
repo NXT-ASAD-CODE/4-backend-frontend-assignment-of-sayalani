7090f24ad2292995114ba8d53574d49f716abcb6
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-17 20:25:09 +0500

    Changing Images Location to check the issue

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,16 +4,16 @@ function Slider() {
   return (
     <div className="slider-container">
       <div className='slider-track'>
-        <img src="../public/Vector (2).png" alt="" />
-        <img src="../public/Vector (3).png" alt="" />
-        <img src="../public/Vector (4).png" alt="" />
-        <img src="../public/Vector (5).png" alt="" />
-        <img src="../public/Vector (6).png" alt="" />
-        <img src="../public/Vector (2).png" alt="" />
-        <img src="../public/Vector (3).png" alt="" />
-        <img src="../public/Vector (4).png" alt="" />
-        <img src="../public/Vector (5).png" alt="" />
-        <img src="../public/Vector (6).png" alt="" />
+        <img src="./Vector (2).png" alt="" />
+        <img src="./Vector (3).png" alt="" />
+        <img src="./Vector (4).png" alt="" />
+        <img src="./Vector (5).png" alt="" />
+        <img src="./Vector (6).png" alt="" />
+        <img src="./Vector (2).png" alt="" />
+        <img src="./Vector (3).png" alt="" />
+        <img src="./Vector (4).png" alt="" />
+        <img src="./Vector (5).png" alt="" />
+        <img src="./Vector (6).png" alt="" />
       </div>
     </div>
   )
