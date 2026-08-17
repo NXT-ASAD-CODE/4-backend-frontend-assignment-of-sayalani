375bd96348f3cf10ff6b8f443a0493aa0f796bcc
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-17 20:16:42 +0500

    Making Slider Working

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 function Slider() {
   return (
-    <div className='Slider'>
+    <div className="slider-container">
+      <div className='slider-track'>
         <img src="../public/Vector (2).png" alt="" />
         <img src="../public/Vector (3).png" alt="" />
         <img src="../public/Vector (4).png" alt="" />
         <img src="../public/Vector (5).png" alt="" />
         <img src="../public/Vector (6).png" alt="" />
+        <img src="../public/Vector (2).png" alt="" />
+        <img src="../public/Vector (3).png" alt="" />
+        <img src="../public/Vector (4).png" alt="" />
+        <img src="../public/Vector (5).png" alt="" />
+        <img src="../public/Vector (6).png" alt="" />
+      </div>
     </div>
   )
 }
