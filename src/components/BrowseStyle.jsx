93f0ed17c580d2317782ef341928dd11233772f1
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-17 19:55:24 +0500

    Placing text on the Image

--- src/components/BrowseStyle.jsx
+++ src/components/BrowseStyle.jsx
@@ -4,19 +4,22 @@ function BrowseStyle() {
     return (
         <div className='flex'>
             <div className="panel">
-                <h1>Browse by Dress Style</h1>
+                <h1 className='panel-heading'>Browse by Dress Style</h1>
                 <div className="style-grid">
                     <div className="style-card card-casual">
-                        <p className='Image-Text'>asad</p>
+                        <h1 className='Image-Text'>Casual</h1>        
                         <img src="../public/image 11.png" alt="Casual style" />
                     </div>
                     <div className="style-card card-formal">
+                        <h1 className='Image-Text'>Formal</h1> 
                         <img src="../public/image 13.png" alt="Formal style" />
                     </div>
                     <div className="style-card card-party">
+                        <h1 className='Image-Text'>Party</h1> 
                         <img src="../public/image 12.png" alt="Party style" />
                     </div>
                     <div className="style-card card-gym">
+                        <h1 className='Image-Text'>Gym</h1> 
                         <img src="../public/image 14.png" alt="Gym style" />
                     </div>
                 </div>
